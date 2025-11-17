0b344694e0212d58c2d78b7716d5b459e4762f56
José Ángel Meléndez Ramos <jmelendez6081@tecnocomfenalco.edu.co>
2025-11-17 07:40:17 +0000

    Add: primera interaccion con terraform exitosa

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_location" {
+  type        = string
+  default     = "eastus"
+  description = "Location of the resource group."
+}
+
+variable "resource_group_name" {
+  type        = string
+  default     = "AIDA-Resource-Group"
+  description = "Name of the resource group."
+}
